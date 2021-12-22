5b0006ace532d679ae7676b632b0fadc5caee7a2
NickolasHKraus <NickHKraus@gmail.com>
2021-12-22 14:45:40 -0600

    Update outputs.tf files

--- examples/static-website/outputs.tf
+++ examples/static-website/outputs.tf
@@ -3,10 +3,10 @@
 # -----------------------------------------------------------------------------
 output "s3_root_id" {
   value       = module.static_website.s3_root_id
-  description = "The name of the root S3 bucket."
+  description = "The name of the root S3 bucket"
 }
 
 output "cf_distribution_id" {
   value       = module.static_website.cf_distribution_id
-  description = "The identifier for the CloudFront distribution."
+  description = "The identifier for the CloudFront distribution"
 }
--- outputs.tf
+++ outputs.tf
@@ -3,10 +3,10 @@
 # -----------------------------------------------------------------------------
 output "s3_root_id" {
   value       = aws_s3_bucket.s3_root.id
-  description = "The name of the root S3 bucket."
+  description = "The name of the root S3 bucket"
 }
 
 output "cf_distribution_id" {
   value       = aws_cloudfront_distribution.distribution.id
-  description = "The identifier for the CloudFront distribution."
+  description = "The identifier for the CloudFront distribution"
 }
