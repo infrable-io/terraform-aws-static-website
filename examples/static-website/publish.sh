2a33b03d1ee5899b696cf51e6346c514a5383201
NickolasHKraus <NickHKraus@gmail.com>
2021-12-22 14:46:02 -0600

    Update publish.sh files

--- examples/static-website/publish.sh
+++ examples/static-website/publish.sh
@@ -5,16 +5,16 @@
 # usage: publish.sh
 
 # set S3 bucket
-S3_BUCKET_ROOT=$(terraform output s3_root_id)
+S3_BUCKET_ROOT=$(terraform output s3_root_id | tr -d \")
 
 # set CloudFront distribution ID
-CF_DISTRIBUTION_ID=$(terraform output cf_distribution_id)
+CF_DISTRIBUTION_ID=$(terraform output cf_distribution_id | tr -d \")
 
 # remove files from S3
-aws s3 rm s3://$S3_BUCKET_ROOT --recursive
+aws s3 rm "s3://${S3_BUCKET_ROOT}" --recursive
 
 # sync files with S3
-aws s3 sync --acl "public-read" site/ s3://$S3_BUCKET_ROOT
+aws s3 sync --acl "public-read" site/ "s3://${S3_BUCKET_ROOT}"
 
 # invalidate CloudFront cache
-aws cloudfront create-invalidation --distribution-id $CF_DISTRIBUTION_ID --paths "/*"
+aws cloudfront create-invalidation --distribution-id "${CF_DISTRIBUTION_ID}" --paths "/*"
